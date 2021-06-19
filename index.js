182eceffcb951e6de955129c9662a7e3369337ff
Rehan959 <rehanshami007@gmail.com>
2021-06-19 06:30:19 +0530

    Grade calculator

--- index.js
+++ index.js
@@ -33,8 +33,7 @@ else if(per <= 70)
     Grade.style.color="yellow";
     percText.style.color="yellow";
 }
-if(per <= 40)
-{
+if(per <= 40){
     Grade.innerHTML="Your Grade is : D";
     Grade.style.color="red";
     percText.style.color="red";
